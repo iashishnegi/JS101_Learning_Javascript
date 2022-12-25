4e5816cfed0b762e67e0af30d8aea09b06c6c650
iashishnegi <the.fzs.kid@gmail.com>
2022-12-25 23:44:26 +0530

    Pushing Introduction to Javascript Problems

---
+++ problem2.js
@@ -0,0 +1,6 @@
+let name = "Ashish";
+console.log(name);
+let fname = "Rajender";
+console.log(fname);
+let mname = "Nirmala";
+console.log(mname);
---
+++ problem4.js
@@ -0,0 +1,34 @@
+let details = "𝙎𝙩𝙪𝙙𝙚𝙣𝙩 𝘿𝙚𝙩𝙖𝙞𝙡𝙨 : "
+
+let n = "𝑁𝑎𝑚𝑒     ~ ";
+let sc = "𝑆𝑐ℎ𝑜𝑜𝑙   ~ ";
+let g = "𝐺𝑟𝑎𝑑𝑒    ~ ";
+let se = "𝑆𝑒𝑐𝑡𝑖𝑜𝑛  ~ ";
+let r = "𝑅𝑜𝑙𝑙 𝑛𝑜. ~ ";
+let n1 = "𝐴𝑠ℎ𝑖𝑠ℎ";
+let sc1 = "𝐶𝐵𝑆𝐸";
+let g1 = "12𝑡ℎ";
+let se1 = "𝐴";
+let r1 = "25";
+let br = "         "
+
+let report = "𝙍𝙚𝙥𝙤𝙧𝙩  𝘾𝙖𝙧𝙙 : "
+let s1 = "𝑀𝑎𝑡ℎ𝑠   = ";
+let s2 = "𝐸𝑛𝑔𝑙𝑖𝑠ℎ = ";
+let s3 = "𝑆𝑐𝑖𝑒𝑛𝑐𝑒 = ";
+let m1 = "92";
+let m2 = "87";
+let m3 = "78";
+
+console.log(details);
+console.log(n, n1);
+console.log(sc, sc1);
+console.log(g, g1);
+console.log(se, se1);
+console.log(r, r1);
+
+console.log(br);
+console.log(report);
+console.log(s1, m1);
+console.log(s2, m2);
+console.log(s3, m3);
